a87b7c3339a0e327af195fb40248378fe34f2e6c
Samuel Alene <smlalene@gmail.com>
2022-07-04 08:02:35 -0700

    add task 11

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 10-square.py
+# 11-square.py
 
 """Defines a Rectangle subclass Square."""
 Rectangle = __import__('9-rectangle').Rectangle
@@ -15,4 +15,4 @@ class Square(Rectangle):
         """
         self.integer_validator("size", size)
         super().__init__(size, size)
-        self.__size = size
+        self.__size = 
